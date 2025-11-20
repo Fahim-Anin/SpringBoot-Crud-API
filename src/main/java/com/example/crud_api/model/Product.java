3f737e01b9eb95ab65f791a012ba5111ea135c25
Fahim-Anin <fahimmuntasir040@gmail.com>
2025-11-21 01:30:50 +0600

    5TH  commit

--- src/main/java/com/example/crud_api/model/Product.java
+++ src/main/java/com/example/crud_api/model/Product.java
@@ -1,36 +1,29 @@
 package com.example.crud_api.model;
 
 import jakarta.persistence.*;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import org.springframework.stereotype.Component;
-
 import java.util.Objects;
 
-//@Component
-@Entity  // It is because Product is the entity class
+@Entity
 public class Product {
 
     @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int prodId;
-
     private String prodName;
     private int prodPrice;
 
     public Product() {
-
+        // Required for JPA
     }
-    public Product(int prodId , String prodName, int prodPrice ) {
 
+    // Constructor for convenience (optional, but good practice)
+    public Product(int prodId , String prodName, int prodPrice ) {
         this.prodId = prodId;
         this.prodName = prodName;
         this.prodPrice = prodPrice;
-
-
     }
 
+    // --- GETTERS ---
     public int getProdId() {
         return prodId;
     }
@@ -43,6 +36,21 @@ public class Product {
         return prodPrice;
     }
 
+    // --- SETTERS (CRITICAL FIX) ---
+    // You do not need a setter for prodId if it's auto-generated,
+    // but you need one to set the object's state if you pass an ID for an update.
+    public void setProdId(int prodId) {
+        this.prodId = prodId;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    public void setProdPrice(int prodPrice) {
+        this.prodPrice = prodPrice;
+    }
+
     @Override
     public String toString() {
         return "Product{" +
@@ -52,17 +60,16 @@ public class Product {
                 '}';
     }
 
-    public boolean equals(Object o)
-    {
-        if(o == null || this.getClass() != o.getClass())
-        {
-            return false;
-        }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return prodId == product.prodId;
+    }
 
-        Product p = (Product) o;
-        return prodId == p.prodId;
-    }
+    @Override
     public int hashCode() {
-        return Objects.hash(prodId); // equals মেথড ওভাররাইড করলে hashCode অবশ্যই করতে হবে
+        return Objects.hash(prodId);
     }
-    }
+}
